e97435dff959c025297197ce64b29318cd6f987e
sonali komati <sonalikomati629@gmail.com>
2026-03-11 21:55:09 +0530

    Removed old login_ui_app project

--- login_ui_app/android/app/src/main/kotlin/com/example/login_ui_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.login_ui_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- login_ui_app/lib/models/shopping_item.dart
+++
@@ -1,6 +0,0 @@
-class ShoppingItem {
-  final String name;
-  int quantity;
-
-  ShoppingItem({required this.name, this.quantity = 1});
-}
